8a601c4ccee6e756394c07351a9174cc25822654
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-19 02:43:07 -0600

    implement routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { HelloWorld } from './Page/HelloWorld';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Error } from './routes/root/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <Error />,
+    element: (
+      <div>
+        <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+          Hello world!
+        </h1>
+      </div>
+    )
+  },
+  {
+    path: 'bye',
+    element: (
+      <div>
+        <h1 className="bg-red-700 px-4 py-2 text-white hover:bg-red-800 sm:px-8 sm:py-3">
+          Bye world!
+        </h1>
+      </div>
+    )
+  }
+]);
 
 function App() {
-  return (
-    <div>
-      <HelloWorld />
-      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        Hello world!
-      </h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Page/HelloWorld.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './HelloWorld.module.css';
-
-export const HelloWorld = () => {
-  return <div className={styles.HelloWorld}>Hello World</div>;
-};
