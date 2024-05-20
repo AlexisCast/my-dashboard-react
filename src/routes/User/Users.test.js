d78f4d147c9d4bb4ab207ff59deb759260796d20
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-05-19 23:30:03 -0600

    implement testing on Users

---
+++ src/routes/User/Users.test.js
@@ -0,0 +1,26 @@
+import { render, screen, act, within } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import Users from './Users';
+
+test('it can receive a new user and show it on a list', async () => {
+  render(<Users />);
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  await user.click(nameInput);
+  await user.keyboard('Baz');
+  await user.click(emailInput);
+  await user.keyboard('Baz@email.com');
+
+  await user.click(button);
+
+  // screen.debug();
+
+  const name = screen.getByRole('cell', { name: 'Baz' });
+  const email = screen.getByRole('cell', { name: 'Baz@email.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+});
