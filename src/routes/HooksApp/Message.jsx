3c14e4550561eadc665f77bc63763d44ac6cf516
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-08-10 18:15:10 -0600

    feat 16: Hooks - useEffect Precautions

--- src/routes/HooksApp/Message.jsx
+++ src/routes/HooksApp/Message.jsx
@@ -1,17 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     console.log('Message component mounted');
 
+    const onMouseMove = ({ x, y }) => {
+      // const coords = { x, y };
+      // console.log('onMouseMove', coords);
+      setCoords({ x, y });
+    };
+
+    window.addEventListener('mousemove', onMouseMove); //by reference
+
     return () => {
       console.log('Message component unmounted');
+
+      window.removeEventListener('mousemove', onMouseMove); //by reference
     };
   }, []);
 
   return (
     <>
       <h3>User alreay exist</h3>
+      {JSON.stringify(coords)}
     </>
   );
 };
